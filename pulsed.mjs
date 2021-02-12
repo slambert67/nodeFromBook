395f27a745aaf8c1f53710ab1b3f422bb7f5913b
Steve Lambert <steve.lambert67@gmail.com>
2021-02-12 18:25:53 +0000

    Refined event emitter

--- pulsed.mjs
+++ pulsed.mjs
@@ -1,11 +1,15 @@
+// this import only works if ./pulser.mjs has a NAMED export called Pulser
 import { Pulser } from './pulser.mjs';
 
 // instantiate a Pulser object
 const pulser = new Pulser();
 
-// Handler function
+// Event handler functions
 pulser.on('pulse', () => {
     console.log(`${new Date().toISOString()} pulse received`);
+});
+pulser.on('pulsewithdata', (p1) => {
+    console.log(`${new Date().toISOString()} pulse received with ${p1}`);
 });
 
 // Start it pulsing
--- pulser.mjs
+++ pulser.mjs
@@ -1,11 +1,27 @@
-import EventEmitter from 'events';
+/**
+ * ES6 module identifier is actually a URL
+ * Here we are importing from a node core module - see https://nodejs.org/api/index.html
+ */
+import EventEmitter from 'events';  // https://github.com/nodejs/node/blob/v15.8.0/lib/events.js
 
+// NAMED export
 export class Pulser extends EventEmitter {
+    // class function
     start() {
+        // var self = this; - no longer needed
+
+        // setInterval provided by global node object - executes provided callback at a specified interval
         setInterval( () => {
-            //console.log(`${new Date().toISOString()} >>>> pulse`);
-            this.emit('pulse');
-            //console.log(`${new Date().toISOString()} <<<< pulse`);
+            /**
+             * 'this' refers to Pulser object instance
+             * Before arrow functions, 'this' would have referred to some other object related to setInterval
+             * See discussion of 'this' in techAndTools repo
+             */
+
+            // self.emit('pulse'); - no longer needed
+
+            this.emit('pulse');          // 'pulse' is the name of the event
+            this.emit('pulsewithdata', 'the data');  // 'pulsewithdata' is the name of the event
         }, 3000)
     }
 }

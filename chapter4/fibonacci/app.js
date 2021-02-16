0f5dc65002114be2d6c43c7c2d777c3f79559351
Steve Lambert <steve.lambert67@gmail.com>
2021-02-16 19:39:56 +0000

    Added inefficient Fibonacci

--- chapter4/fibonacci/app.js
+++ chapter4/fibonacci/app.js
@@ -1,17 +1,19 @@
 var createError = require('http-errors');
 var express = require('express');
+const hbs = require('hbs');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const fibonacciRouter = require('./routes/fibonacci');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, 'partials'));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);  // mounts router object on /users so '/' refers to /users
+app.use('/fibonacci', fibonacciRouter);
 
 // catch 404 and forward to error handler
 // middleware raising error will have called next() with 1st parameter being the error

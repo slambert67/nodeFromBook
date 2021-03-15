61bccf4c37e055df40fa3edabc8a1ff902e071cd
Steve Lambert <steve.lambert@ultra-as.com>
2021-03-15 17:08:15 +0000

    Add async await

--- chapter4/fibonacci/promises/promise1.js
+++ chapter4/fibonacci/promises/promise1.js
@@ -162,7 +162,7 @@ console.log('End of promise chain');
 // Can have multiple catches in promise chain (e.g. for logging) but the promise returned is not rejected! Must rethrow the error to propagate
 
 // reject with reject
-var rejectedPromise = new Promise( function(resolve,reject){
+/*var rejectedPromise = new Promise( function(resolve,reject){
     reject( new Error('Arghh!') );  // explicit rejection
 })
 .then( (res) => {console.log('do not get here')} )
@@ -173,5 +173,66 @@ var rejectedPromise2 = new Promise( function(resolve,reject){
     throw new Error('Arghh2!');  // implicit rejection
 })
 .then( (res) => {console.log('do not get here')} )
-.catch( (e) => {console.log('rejected implicitly'); });
+.catch( (e) => {console.log('rejected implicitly'); });*/
 ////////////////////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// async/await
+////////////////////////////////////////////////////////////////////////////////
+// avoids the need to explicitly configure promise chains.
+// First of all we have the async keyword, which you put in front of a function declaration to turn it into an async function
+// An async function is a function that knows how to expect the possibility of the await keyword being used to invoke asynchronous code
+// return values are guaranteed to be converted to promises.
+// So the async keyword is added to functions to tell them to return a promise rather than directly returning the value.
+// The advantage of an async function only becomes apparent when you combine it with the await keyword.
+// await can be put in front of any async promise-based function to pause your code on that line until the promise fulfills, then return the resulting value.
+// Instead of needing to chain a .then() block on to the end of each promise-based method, you just need to add an await keyword before the method call, and then assign the result to a variable.
+// The await keyword causes the JavaScript runtime to pause your code on this line, not allowing further code to execute in the meantime until the async function call has returned its result — very useful if subsequent code relies on that result!
+
+/*
+async function myAsyncTest() {
+    // invoke asynchronous code
+    let innerResult = await rp('http://localhost:3002/fibonacci/10');
+    console.log(innerResult);
+}
+console.log('Calling myAsyncTest;');
+let outerResult = myAsyncTest();
+console.log('Called myAsyncTest;');
+console.log(outerResult);
+*/
+let summation = "X";
+/*
+rp('http://localhost:3002/fibonacci/10')
+    .then( (res) => {
+        summation = summation + "X";
+        console.log(summation);
+    })
+    .then( (res) => {
+        summation = summation + "X";
+        console.log(summation);
+    })
+    .then( (res) => {
+        summation = summation + "X";
+        console.log(summation);
+    })
+    .then( (res) => {
+        summation = summation + "X";
+        console.log(summation);
+    })
+    .catch( (e) => {
+        console.log('promise rejected');
+    });
+    */
+
+async function doSummation() {
+    let result = await rp('http://localhost:3002/fibonacci/10');
+    let result2 = await result;
+    let result3 = await result2;
+    console.log('final result = ' + result3);
+}
+
+doSummation();
+console.log('done summation');
+
